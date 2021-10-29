f5aaeda270bd68cec4a8a9f741e90859e197bfdf
fransoa2103 <73296404+fransoa2103@users.noreply.github.com>
2021-10-29 12:49:59 +0200

    design xl-screen improved

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 "use strict";
 let current_year = new Date();
 let copyRight = document.getElementById('copyright');
-copyRight.innerText="website by FCB ©" + current_year.getFullYear();
+copyRight.innerText="website by FCB-WEB ©" + current_year.getFullYear();
